1c10ee7ca0732302aca5d4778a3b6a7841518a18
AlexNina222 <alexricardoninausquiano@gmail.com>
2026-04-22 11:24:18 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,9 @@ Route::middleware('auth')->group(function () {
     ]);
 
 
+    Route::resource('clientes', ClienteController::class);
+
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
