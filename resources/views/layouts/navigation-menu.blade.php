cd956dff28f14b9c91781d3c5268a0bcc090bd0d
AlexNina222 <alexricardoninausquiano@gmail.com>
2026-04-22 11:28:35 -0400

    Update navigation-menu.blade.php

--- resources/views/layouts/navigation-menu.blade.php
+++ resources/views/layouts/navigation-menu.blade.php
@@ -157,6 +157,24 @@
                     <span class="ml-4">Presentaciones</span>
                 </a>
             </li>
+
+ </li>
+
+                        <li class="relative px-6 py-3">
+                @if (request()->routeIs('clientes*'))
+                    <span class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+                        aria-hidden="true"></span>
+                @endif
+                <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150
+                    {{ request()->routeIs('clientes*') ? 'text-gray-800 dark:text-gray-100' : 'text-gray-600 dark:text-gray-400'}}
+                     hover:text-gray-800 dark:text-gray-200"
+                    href="{{route('clientes.index')}}">
+                    <i class="fa-solid fa-layer-group"></i>
+                    <span class="ml-4">Clientes</span>
+                </a>
+            </li>
+
+            
             
         </ul>
       
