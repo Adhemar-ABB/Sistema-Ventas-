e022a932dc47d3cb7d63c91d2a3ef119f9506f38
MakotoSan123 <floresjoseevert281@gmail.com>
2026-04-21 23:28:21 -0400

    Solución de errores de base de datos y vistas

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\MarcaController;
+use App\Http\Controllers\PresentacionController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+
+    Route::resources([
+        'categorias' => CategoriaController::class,
+        'marcas' => MarcaController::class,
+        'presentaciones' => PresentacionController::class,
+    ]);
+
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
